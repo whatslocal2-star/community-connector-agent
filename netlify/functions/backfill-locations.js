6c0579fb97ad8f47245a59164a755a0c430e7197
John Xen <johnxen520@gmail.com>
2026-05-06 22:32:21 -0700

    chore: add backfill-locations admin endpoint

---
+++ netlify/functions/backfill-locations.js
@@ -0,0 +1,31 @@
+import { loadAllMembers, saveMember } from "./lib/db.js";
+import { parseGoogleMapsUrl } from "./lib/parseLocation.js";
+
+export const handler = async (event) => {
+  const token = event.headers.authorization?.replace("Bearer ", "");
+  if (token !== process.env.ADMIN_TOKEN) {
+    return { statusCode: 401, body: "Unauthorized" };
+  }
+
+  const members = await loadAllMembers(500);
+  const needsBackfill = members.filter(
+    (m) => m.profile?.googleMapsUrl && !m.profile?.latitude
+  );
+
+  const results = [];
+  for (const m of needsBackfill) {
+    const coords = await parseGoogleMapsUrl(m.profile.googleMapsUrl);
+    if (coords) {
+      await saveMember(m.id, { profileUpdate: coords });
+      results.push({ id: m.id, name: m.profile.name, ...coords });
+    } else {
+      results.push({ id: m.id, name: m.profile.name, error: "could not parse" });
+    }
+  }
+
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ backfilled: results.length, results }),
+  };
+};
